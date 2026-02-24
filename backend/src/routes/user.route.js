fe217d367081f7939ab1e2aece3e5b73ef22ca77
saif <kaifansari1119@gmail.com>
2026-02-24 23:28:58 +0530

    fix: update webhook route paths for consistency

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,9 +4,9 @@ const router = Router();
 import { userData, clerkWebhook } from "../controllers/user.controller.js";
 
 router.route("/").get(userData);
-router.route("/clerk-webhook").post(clerkWebhook);
-router.route("/clerk-webhook").get((req, res) => {
-    res.json({message: "This endpoint is for Clerk webhooks. Please send a POST request."});
+router.route("/webhooks").post(clerkWebhook);
+router.route("/webhooks").get((req, res) => {
+  res.json({ message: "This endpoint is for Clerk webhooks. Please send a POST request." });
 });
 
 export default router;
