c0348bdcfffd2953f2b2cc21ebb49bea5a5333fe
saif <kaifansari1119@gmail.com>
2026-03-22 12:49:41 +0530

    test

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -6,14 +6,11 @@ import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
 
 const app = express()
 
-app.options("*", cors());
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "token"]
-}))
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
