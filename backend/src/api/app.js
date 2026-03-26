36a7aced8aec69c30c61b661cd0a0cadab5f8adf
saif <kaifansari1119@gmail.com>
2026-03-27 01:29:45 +0530

    cors error2

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 // ✅ CORS
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://what-remove-3wds-liard.vercel.app"],
   credentials: true,
 }));
 
